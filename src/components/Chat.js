0ba679d3473fae34feb036d9694a9200c9965f47
Natchapong Rammasute <natchapongrammasute@Natchapongs-MacBook-Pro.local>
2021-10-31 16:48:21 +0700

    adding message box and layout

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,10 +7,50 @@ const Chat = () => {
     const [message, setMessage] = useContext(MessageContext);
 
 
+    //Set uer message to change message 
+    const setUserText = (event) => {
+        const {name, value} = event.target;
+        setMessage(prevValue => ({
+            ...prevValue,
+            [name]: value
+
+        }))
+    }
+
+
     return(
         <div>
-            <h1>Channel : {message.channelId}</h1>
+            <div>
+                <h1>Channel : {message.channelId}</h1>
+            </div>
+            <div className="chatBox">
+                <ul className="chatbox-show-result">
+                    <li className="chat-left">
+                        <button  type="button"  className="btn btn-secondary buttonRead">
+                            Read More <i className="fa fa-arrow-up"></i>
+                        </button>
+                    </li>
+                    {/* SPPOUSE TO SHOW ALL THE MESSAGE HERE  */}
+
+                    <li className="chat-left">
+                        <button  type="button"  className="btn btn-secondary buttonRead">
+                            Read More <i className="fa fa-arrow-up"></i>
+                        </button>
+                    </li>
+                </ul>
+                <div className="chat-message">
+                    <textarea name="text" value={message.text} onChange={setUserText} cols="30" rows="10" placeholder="Type text here"></textarea>
+                    <button  type="button" className="btn btn-primary">
+                        Send Message <i className="fa fa-send"></i>
+                    </button>
+
+                </div>
+
+            </div>
+        
+        
         </div>
+        
         
     )
 }
